48ca133a12b38bbdb9a82b8d9fd77f3e2471457e
21534319 <21534319@N204-04.alumnos.uem.es>
2015-10-20 09:39:28 +0200

    segunda prueba

---
+++ swing/src/swing/gonzalo.java
@@ -0,0 +1,9 @@
+package swing;
+
+public class gonzalo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre="mala piu";
+		System.out.println(nombre);
+	}
+}
